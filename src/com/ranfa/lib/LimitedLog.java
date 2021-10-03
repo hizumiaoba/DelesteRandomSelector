08d36d3fd1600b7de455142625dbf406a53eb304
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2021-10-03 12:24:10 +0900

    perf: added time data to log sentence

--- src/com/ranfa/lib/LimitedLog.java
+++ src/com/ranfa/lib/LimitedLog.java
@@ -1,21 +1,29 @@
 package com.ranfa.lib;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class LimitedLog {
 
+	private final static String STRING_DATE_FORMAT = "YYYY-MM-dd HH-mm-ss-SSS: ";
+	private static SimpleDateFormat format = new SimpleDateFormat(STRING_DATE_FORMAT);
+
 	public static void print(String s) {
+		Calendar calendar = Calendar.getInstance();
 		if(!Settings.outputDebugSentences()) {
 			return;
 		} else {
-			System.out.print(s);
+			System.out.print(format.format(calendar.getTime()) + s);
 			return;
 		}
 	}
 
 	public static void println(String s) {
+		Calendar calendar = Calendar.getInstance();
 		if(!Settings.outputDebugSentences()) {
 			return;
 		}
-		System.out.println(s);
+		System.out.println(format.format(calendar.getTime()) + s);
 	}
 
 	public static void println() {
