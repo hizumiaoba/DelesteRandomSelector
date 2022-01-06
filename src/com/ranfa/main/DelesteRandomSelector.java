8f30829f93c5dc96dc042dc69f8601b025d341de
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-01-06 23:23:02 +0900

    chore: increment version to v3.0.1

--- src/com/ranfa/main/DelesteRandomSelector.java
+++ src/com/ranfa/main/DelesteRandomSelector.java
@@ -43,7 +43,7 @@ import com.ranfa.lib.Song;
 import com.ranfa.lib.TwitterIntegration;
 import com.ranfa.lib.Version;
 
-@Version(major = 3, minor = 0, patch = 0)
+@Version(major = 3, minor = 0, patch = 1)
 public class DelesteRandomSelector extends JFrame {
 
 	private static ArrayList<Song> selectedSongsList = new ArrayList<>();
