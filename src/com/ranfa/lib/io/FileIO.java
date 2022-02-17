ab769ffa67ad7d0bf27b4fc1f52241f6e71cb55d
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-02-17 10:26:14 +0900

    feat: add file out func

---
+++ src/com/ranfa/lib/io/FileIO.java
@@ -0,0 +1,16 @@
+package com.ranfa.lib.io;
+
+public class FileIO {
+	
+	// constants
+	private static final String FILE_NAME_PATTERN = "YYYY-MM-dd-HH-mm-ss";
+	private static final String FILE_EXTENSION = ".drs";
+	
+	// fields
+	private String songName;
+	
+	public FileIO() {
+		
+	}
+
+}
---
+++ src/com/ranfa/lib/io/OutputDataStructure.java
@@ -0,0 +1,13 @@
+package com.ranfa.lib.io;
+
+import java.io.Serializable;
+
+public class OutputDataStructure implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String songName;
+	private int level;
+	private String difficulty;
+	
+}
