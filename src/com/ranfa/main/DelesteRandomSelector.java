ba6adb41ceff88d8c7277dbfdcffc31e7b8667ce
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-04-13 22:02:11 +0900

    chore: increment version to v4.0.3-Beta

--- src/com/ranfa/main/DelesteRandomSelector.java
+++ src/com/ranfa/main/DelesteRandomSelector.java
@@ -63,7 +63,7 @@ import com.ranfa.lib.database.Song;
 import com.ranfa.lib.handler.CrashHandler;
 import com.ranfa.lib.songinfo.FetchFromAPI;
 
-@Version(major = 4, minor = 0, patch = 2, suffix = Suffix.BETA)
+@Version(major = 4, minor = 0, patch = 3, suffix = Suffix.BETA)
 public class DelesteRandomSelector extends JFrame {
 
     private static ArrayList<Song> selectedSongsList = new ArrayList<>();
