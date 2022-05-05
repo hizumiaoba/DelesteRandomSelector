f35d842da91f794ab9aa8c63f1e08d6ae1812615
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-05-05 22:57:50 +0900

    feat: add easter

--- src/test/EasterTest.java
+++ src/test/EasterTest.java
@@ -1,10 +1,15 @@
 package test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.Test;
 
 import com.ranfa.lib.Easter;
+
+import test.mocks.EasterMock;
 
 public class EasterTest {
 
@@ -21,4 +26,28 @@ public class EasterTest {
 		assertNotNull(this.easter.readBirthData());
 	}
 
+	@Test
+	public void specifiedEasterTest() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2020, 6, 13);
+		Date mutsumi = new Date(calendar.toInstant().toEpochMilli());
+		calendar.clear();
+		calendar.set(2020, 9, 27);
+		Date fumika = new Date(calendar.toInstant().toEpochMilli());
+		String mutsumiBirthString = null;
+		String fumikaBirthString = null;
+		try {
+			mutsumiBirthString = EasterMock.getSpecifiedDateBirth(mutsumi);
+			fumikaBirthString = EasterMock.getSpecifiedDateBirth(fumika);
+		} catch (ClassNotFoundException | SecurityException | NoSuchFieldException | IllegalArgumentException
+				| IllegalAccessException e) {
+			fail(e.getMessage());
+		}
+		
+		assertNotNull(mutsumiBirthString);
+		assertNotNull(fumikaBirthString);
+		
+		assertTrue(mutsumiBirthString.contains(" 「お誕生日って、なんだかわくわくしますよねっ！」"));
+		assertTrue(fumikaBirthString.contains(" 「素敵な物語の1頁を、一緒に刻んでいきましょう…プロデューサーさん」"));
+	}
 }
